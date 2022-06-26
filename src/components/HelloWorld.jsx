d4c1ec55007ab39a7e1ebba8d9cfc576358554bf
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-06-25 20:21:04 -0500

    primer proyecto react con vite

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class HellosWorld extends Component {
+  render() {
+    return (
+      <div>Hellos World</div>
+    )
+  }
+}
+
+export default HellosWorld
